9819aa60a340e210fada80c74e4368a93d9a08e5
VladislavKutsobin <v.kutsobin@gms-worldwide.com>
2022-09-28 11:20:47 +0300

    Finished with AuthController

--- src/main/java/com/example/edu/springeducation/controller/AuthController.java
+++ src/main/java/com/example/edu/springeducation/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.example.edu.springeducation.controller;
 
+import com.example.edu.springeducation.model.ERole;
 import com.example.edu.springeducation.model.Role;
+import com.example.edu.springeducation.model.User;
 import com.example.edu.springeducation.payload.request.LoginRequest;
+import com.example.edu.springeducation.payload.request.SignupRequest;
+import com.example.edu.springeducation.payload.response.MessageResponse;
 import com.example.edu.springeducation.payload.response.UserInfoResponse;
 import com.example.edu.springeducation.repository.RoleRepository;
 import com.example.edu.springeducation.repository.UserRepository;
@@ -19,7 +23,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -64,5 +70,62 @@ public class AuthController {
                         userDetails.getEmail(),
                         roles
                 ));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
+        if (userRepository.existsByUsername(signupRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken"));
+        }
+
+        if (userRepository.existsByEmail(signupRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already in use"));
+        }
+
+        User user = new User(signupRequest.getUsername(),
+                signupRequest.getEmail(),
+                passwordEncoder.encode(signupRequest.getPassword()));
+
+        Set<String> strRoles = signupRequest.getRole();
+        Set<Role> roles = new HashSet<>();
+
+        if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            strRoles.forEach(role -> {
+                switch (role) {
+                    case "admin":
+                        Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(adminRole);
+
+                        break;
+                    case "mod":
+                        Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(modRole);
+
+                        break;
+                    default:
+                        Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                                .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                        roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+    }
+
+    @PostMapping("/signout")
+    public ResponseEntity<?> logoutUser() {
+        ResponseCookie cookie = jwtUtils.getCleanJwtCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new MessageResponse("You have been signed out"));
     }
 }
--- src/main/java/com/example/edu/springeducation/model/User.java
+++ src/main/java/com/example/edu/springeducation/model/User.java
@@ -40,7 +40,7 @@ public class User {
 
     }
 
-    public User(String username, String password, String email) {
+    public User(String username, String email, String password) {
         this.username = username;
         this.password = password;
         this.email = email;
